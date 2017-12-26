bc6f3bd21f7282d3cea104cbdc810a42f3762aaa
macieg-b <bartlomiejczyk.maciej@gmail.com>
2017-12-26 21:41:36 +0100

    initial commit

---
+++ src/main/java/mba/patronagebe/PatronageBeApplication.java
@@ -0,0 +1,12 @@
+package mba.patronagebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatronageBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatronageBeApplication.class, args);
+	}
+}
